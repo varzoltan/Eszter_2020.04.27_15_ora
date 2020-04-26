0cbfc8abb46825130a3fccd7f9f91fe266692f67
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-04-26 19:46:56 +0200

    Update Program.cs

--- Eszter_2020.04.27_15_ora/Program.cs
+++ Eszter_2020.04.27_15_ora/Program.cs
@@ -10,13 +10,7 @@ namespace Eszter_2020._04._27_15_ora
     {
         static void Main(string[] args)
         {
-            char kar = 'b', kar1 = 'c';
-            Console.WriteLine('\a');
-            Console.Write(kar+kar1+"\t\f");
-            Console.Write("szöveg!");
-            Console.WriteLine('\b');
-            Console.WriteLine('\u0066');
-            Console.ReadKey();
+            
         }
     }
 }
